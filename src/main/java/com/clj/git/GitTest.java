286e04ebed748059be387cf84c007ae61f63c886
maoke <maoke@guigu.com>
2023-01-08 16:39:53 +0800

    first commit

---
+++ src/main/java/com/clj/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.clj.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author xinchao
+ * @Date 2023/1/8 16:37
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
